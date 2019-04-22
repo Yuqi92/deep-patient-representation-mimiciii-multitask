3877a68d8cf2f58b62866924ed05271f5a4626ab
Yuqi92 <ys2887@columbia.edu>
2019-04-22 09:42:22 -0500

    Update HP.py
    add comments

--- HP.py
+++ HP.py
@@ -1,9 +1,9 @@
+# coding=utf-8
 
-# if we set insight_patient_id to None, it means that we do not want to get insight of patient
-insight_patient_id = "patient1231"
-use_everything_to_test = True
+"""parameter used in the project"""
 
-# data
+
+# mimic data
 mimic_note_events = 'mimic_csv/NOTEEVENTS.csv'
 mimic_admissions = 'mimic_csv/ADMISSIONS.csv'
 mimic_patients = 'mimic_csv/PATIENTS.csv'
@@ -20,6 +20,9 @@ def get_data_directory():
 
 patient_vector_directory = "merged/patient_vector/patient_vector_3_task/"
 
+# if we set insight_patient_id to None, it means that we do not want to get insight of patient
+insight_patient_id = "patient1231"
+use_everything_to_test = True
 
 result_csv = "merged/file/result_csv_dead_los.csv"
 insight_patient_result_csv = "/merged/file_" + insight_patient_id + "/result_csv_dead_los.csv"
@@ -31,6 +34,7 @@ def get_result_csv():
     else:
         return insight_patient_result_csv
 
+
 #subject_index = "/merged/file/subject.csv"
 
 index_path = 'merged/index'
@@ -41,12 +45,13 @@ index_test_path = index_path + '/test.npy'
 load_index = True
 read_data_thread_num = 8
 
+#pre-trained embedding
 embedding_file = '/home/ysi/Documents/amia/cancer_ner_relation_v1/data/glove.6B/mimic.k100.w2v'
 
 n_max_sentence_num = 1000 # truncated to 1000 sentences a document
 n_max_word_num = 25 # truncated to 25 words a sentence
 
-# category
+# note category
 category = ['pad', 'Respiratory', 'ECG','Radiology','Nursing/other','Rehab Services','Nutrition','Pharmacy','Social Work',
             'Case Management','Physician','General','Nursing','Echo','Consult']
 category_id = {cate: idx for idx, cate in enumerate(category)}
@@ -64,7 +69,8 @@ tasks_dead_date = [366]
 
 # length of stay
 tasks_los_date = []
-# model
+
+# CNN model hyperparameters
 restore = True
 multi_size = len(tasks_dead_date) + len(tasks_los_date)
 embedding_size = 100
@@ -82,25 +88,20 @@ learning_rate = 0.001
 rate_decay = 0.99
 drop_out_train = 0.8
 
-
-model_type = "CNN"  # can be CNN or SIMPLE
-
+model_type = "CNN"  # option: can be CNN or SIMPLE Feedforward NN
 
 num_classes = 2
 lambda_regularizer_strength = 5
 
 model_folder = "merged/multi_task/result_3_task/model_1/"
 model_path = model_folder+ "model.weights/model.ckpt"
-#model_path = "merged/multi_task/result_3_task/model_1/model.weights/model.ckpt"
-#out_hosp/results/uni_task_in_month/model_1/model.weights/model.ckpt"
-# CNN_mimiciii_mortality_prediction/merged/multi_task/result_20_task/model_1
+
 # log
 log_file_name = "example.log"
 
-
 test_output = model_folder +'sentence_error_analysis_single_task_' + insight_patient_id + '.csv'
 
-
+# tensorboard tsne visualization
 tsne_vector_directory = 'merged/patient_vector/visualization/3_task/vector.npy'
 
 tsne_label_directory = 'merged/patient_vector/visualization/3_task/label.txt'
